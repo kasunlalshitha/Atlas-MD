898ad299189576db3eab8a68391790526024c810
kasunlalshitha <130716510+kasunlalshitha@users.noreply.github.com>
2023-05-21 23:11:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ if (!gg) {
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
-global.sessionId = process.env.SESSION_ID || "ok";
+global.sessionId = process.env.SESSION_ID || "0701253022";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
