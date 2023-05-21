3961034a1bd9347c948add0fae563d38ad03ce3d
kasunlalshitha <130716510+kasunlalshitha@users.noreply.github.com>
2023-05-21 22:57:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "942954045";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
